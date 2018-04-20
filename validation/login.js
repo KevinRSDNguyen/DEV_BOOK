faa611c6247d61a79cc0c2819157ceaa8ef6f74c
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-04-20 13:43:37 -0700

    Finished Serverside validation for register/login routes

---
+++ validation/login.js
@@ -0,0 +1,21 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(
+  { email = '', password = '' }
+) {
+  let errors = {};
+
+  if (!Validator.isEmail(email)) { //handles no email submitted or incorrect
+    errors.email = 'Valid email address is required';
+  }
+
+  if (Validator.isEmpty(password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+}
