6f391ea9683e3b29cf0b0d7778d3b9692c0227b5
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-04-19 20:19:25 -0700

    Implemented JWT Authentication Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('./../../config/keys');
+const passport = require('passport');
 
 //Load User Model
 const User = require('../../models/User');
@@ -81,5 +82,14 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { id, name, email } = req.user;
+  res.json({ id, name, email });
+});
+//The PP midware above will send unauthorized if not authenticated.
 
 module.exports = router;
