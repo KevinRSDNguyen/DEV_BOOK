4be2a391bbbe540721feaef57ae676c389ad0150
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-04-21 17:46:44 -0700

    CReated the Post Create Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post Model
+const Post = require("./../../models/Post");
+
+// Validation
+const validatePostInput = require("./../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
-router.get('/test', (req, res) => {
-  res.json({ msg: 'Posts works' });
+router.get("/test", (req, res) => {
+  res.json({ msg: "Posts works" });
 });
+
+// @route   POST api/posts/
+// @desc    Create Post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      //likes and comments will be created as empty arrays by default
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => {
+      res.json(post);
+    });
+  }
+);
 
 module.exports = router;
