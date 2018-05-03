9d49cd590008e9b52f4efca7120a0432d383a435
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-05-02 19:13:37 -0700

    Added error handler for AddExperieince route if user does not have profile yet

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -191,21 +191,26 @@ router.post(
     }
 
     Profile.findOne({ user: req.user.id }).then(profile => {
-      const newExp = {
-        title: req.body.title,
-        company: req.body.company,
-        location: req.body.location,
-        from: req.body.from,
-        to: req.body.to,
-        current: req.body.current,
-        description: req.body.description
-      };
+      if (profile) {
+        const newExp = {
+          title: req.body.title,
+          company: req.body.company,
+          location: req.body.location,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
 
-      // Add to exp array
-      profile.experience.unshift(newExp);
-      profile.save().then(profile => {
-        res.json(profile);
-      });
+        // Add to exp array
+        profile.experience.unshift(newExp);
+        profile.save().then(profile => {
+          res.json(profile);
+        });
+      } else {
+        errors.noprofile = "You do not have a profile to add an experience to.";
+        res.status(400).json(errors);
+      }
     });
   }
 );
