781da9816ec69286fa1030b4433c7e2f8231ec43
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-05-07 19:53:28 -0700

    Finished the PostItem component

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+
+class PostItem extends Component {
+  onDeleteClick = id => {
+    alert(id);
+  };
+  render() {
+    const { post, auth } = this.props;
+
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={post.avatar}
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">{post.name}</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">
+              {post.text.split("\n").map((p, index) => {
+                return (
+                  <span key={index} style={{ display: "block" }}>
+                    {p}
+                  </span>
+                );
+              })}
+            </p>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge badge-light">{post.likes.length}</span>
+            </button>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-secondary fas fa-thumbs-down" />
+            </button>
+            <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+              Comments
+            </Link>
+            {post.user === auth.user.id ? (
+              <button
+                onClick={() => this.onDeleteClick(post._id)}
+                type="button"
+                className="btn btn-danger mr-1"
+              >
+                <i className="fas fa-times" />
+              </button>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth //So users can only delete their posts
+  };
+};
+
+export default connect(mapStateToProps)(PostItem);
